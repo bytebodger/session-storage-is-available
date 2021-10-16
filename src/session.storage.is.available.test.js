9380faca403de6c044ff43fc06c9331b424274aa
Giraficode <giraficode@gmail.com>
2021-10-16 15:00:58 +0000

    add test

--- src/session.storage.is.available.test.js
+++ src/session.storage.is.available.test.js
@@ -1,5 +1,17 @@
-import { sessionStorageIsAvailable } from './session.storage.is.available';
+import { sessionStorageIsAvailable } from "./session.storage.is.available"
 
+import * as session_storage_is_available from "./session.storage.is.available"
 test('sessionStorage is available in the jest console', () => {
    expect(sessionStorageIsAvailable()).toEqual(true);
 });
+
+// @ponicode
+describe("session_storage_is_available.sessionStorageIsAvailable", () => {
+    test("0", () => {
+        let callFunction = () => {
+            session_storage_is_available.sessionStorageIsAvailable()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
